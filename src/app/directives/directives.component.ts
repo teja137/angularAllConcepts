4a7dbd533fa78bf5badf764a3a1b40c9406f95e3
teja <ajetdream@yahoo.com>
2022-04-18 23:15:45 +0530

    3

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent implements OnInit {
 yes = true;
 no = false;
+number=null;
   constructor() { }
 
   ngOnInit(): void {
